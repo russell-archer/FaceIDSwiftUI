971ff9c369d00e0f1f18827ac85a41722611e1ea
Russell Archer <russell.archer@mac.com>
2022-08-03 16:17:35 +0100

    Initial Commit

---
+++ FaceIdSwiftUI/FaceIdSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIdSwiftUIApp.swift
+//  FaceIdSwiftUI
+//
+//  Created by Russell Archer on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FaceIdSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
